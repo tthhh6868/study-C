49d699e2ab472ea0cde51350e9c97135496223e3
tthhh6868 <tthhh6868@outlook.com>
2022-11-29 20:12:37 +0800

    Create game.h

---
+++ Trimoku/game.h
@@ -0,0 +1,21 @@
+#pragma once
+#pragma once
+#include<stdio.h>
+
+#include<time.h>
+
+#include<stdlib.h>
+
+#define ROW 3 
+#define COL 3
+
+//初始化棋盘
+void InitBoard(char board[ROW][COL], int row, int col);
+//打印棋盘
+void DisplayBoard(char board[ROW][COL], int row, int col);
+//玩家下棋
+void PlayerMove(char board[ROW][COL], int row, int col);
+//电脑下棋
+void ComputerMove(char board[ROW][COL], int row, int col);
+//判断输赢
+char IsWin(char board[ROW][COL], int row, int col);
